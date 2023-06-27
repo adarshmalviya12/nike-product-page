9b7ac95179899b95f28e4afa52376a064142a46a
Adarsh Malviya <adarshmalviya1199@gmail.com>
2023-06-27 16:41:56 +0530

    intial commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./App.css";
+import Nav from "./components/Nav";
+
+const App = () => {
+ return (
+  <div className="App">
+   <Nav />
+  </div>
+ );
+};
+
+export default App;
